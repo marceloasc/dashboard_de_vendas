3bdf45fceb08c2468f57ea67a34f912789ec5f97
Marcelo Cardoso <marcelosouza7r@gmail.com>
2021-09-11 17:55:53 -0300

    Integração do DataTable feita

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,3 +1,26 @@
+import { Seller } from './seller';
+
+export type Sale = {
+    id: number;
+    visited: number;
+    deals: number;
+    amount: number;
+    date: string;
+    vendedor: Seller;
+}
+
+export type SalePage = {
+    content?: Sale[];
+    last: boolean;
+    totalPages: number;
+    totalElements: number;
+    size?: number;
+    number: number;
+    first: boolean;
+    numberOfElements?: number;
+    empty?: boolean;
+}
+
 export type SaleSum = {
     nomeVendedor: string;
     total: number;
